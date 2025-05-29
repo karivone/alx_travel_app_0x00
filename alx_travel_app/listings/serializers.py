2ac63e5dffcd3a4ab30c110e823add146c8380b4
karivone <karimiyvonne19@gmail.com>
2025-05-29 19:21:40 +0300

    travel app

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Listing, Booking, Review
+from django.contrib.auth.models import User
 
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
