3112fa8531c68d696738ed7174a874b0452cdb36
karivone <karimiyvonne19@gmail.com>
2025-05-29 19:19:01 +0300

    travel app

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Listing, Booking
+from .models import Listing, Booking, Review
 
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
